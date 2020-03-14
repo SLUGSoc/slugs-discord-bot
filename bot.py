1dd109c4a9b2c3d7c87ff3990fc89fcf9ecf561b
RaptureAwaits <fredericsmythe512@gmail.com>
2020-03-14 10:21:11 +0000

    Welcome message and 'open sesame' now works

--- bot.py
+++ bot.py
@@ -19,7 +19,26 @@ async def on_member_join(member):
 	name = member.name
 	await member.send(botinfo.welcome(name))
 
-
+@client.event
+async def on_message(message):
+	if message.author == client.user: # No  R E C U R S I O N  allowed
+		return
+	
+	channel = message.channel
+	content = message.content
+	
+	member = message.author
+	name = member.name
+	nick = member.display_name
+	
+	if 'would you kindly' in content.lower():
+		if channel == client.get_channel(botinfo.join_channel_id):
+			guild = client.get_guild(botinfo.guild_id)
+			member_role = guild.get_role(botinfo.member_role_id)
+			
+			roles = member.roles
+			if member_role not in roles:
+				await member.add_roles(member_role)
 
 token = botinfo.token
 client.run(token)
--- botinfo_TEMPLATE.py
+++ botinfo_TEMPLATE.py
@@ -1,4 +1,9 @@
-token = "[BOT TOKEN GOES HERE]"
+#Customise fields, then rename to botinfo.py
+token = "" # Bot token from Discord developer dashboard goes here (str)
+guild_id = # (int)
+join_channel_id = # (int)
+bot_channel_id = # (int)
+member_role_id = # (int)
 
 def welcome(name):
-	return "[WELCOME MESSAGE GOES HERE]".format(name)
+	return "".format(name) # Welcome message users are sent on joining goes here (str)
