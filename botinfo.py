76eee8e8ae8690beb27a28aa69384432680e1f0e
RaptureAwaits <fredericsmythe512@gmail.com>
2020-03-14 07:43:36 +0000

    Removed botinfo from repo

--- botinfo.py
+++
@@ -1,4 +0,0 @@
-token = "Njg4Mjc3MDE3NTEwMDE5MTE2.Xmx-Fg.noM6AzeBDpxT6uTmlcm2DjZFPsc"
-
-def wm(name):
-	return "Hi there **{0}!**. Welcome to the SLUGSoc Discord server.\n\nPlease familiarise yourself with the server rules:\n**1)** No hate speech, racism, sexism, or anything that violates Sheffield Students' Union's inclusions policy.\n**2)** Do not harrass other members.\n**3)** No spam (audio, text, links).\n**4)** No pornography, gore, or any other NSFW content.\n**5)** Do not post unmarked spoilers.".format(name)
