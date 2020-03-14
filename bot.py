f2b16ab924c4172699a777ce03473152f432c4ad
RaptureAwaits <fredericsmythe512@gmail.com>
2020-03-14 07:40:34 +0000

    Added server welcome message and botinfo file

--- bot.py
+++ bot.py
@@ -1,2 +1,25 @@
 import discord
+import botinfo
+import sqlite3
+import time
+import os
 
+start_time = time.localtime()
+print(os.getcwd())
+print(time.asctime(start_time))
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print("Ready.")
+
+@client.event
+async def on_member_join(member):
+	name = member.name
+	await member.send(botinfo.wm(name))
+
+
+
+token = botinfo.token
+client.run(token)
